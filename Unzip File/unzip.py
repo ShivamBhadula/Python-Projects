a4dc719b2e51f060c0b9b1de9775795f4b1c2da2
ShivamBhadula <lazycoder10@gmail.com>
2021-11-18 16:05:34 +0530

    upd

--- Unzip File/unzip.py
+++ Unzip File/unzip.py
@@ -2,5 +2,5 @@ import zipfile
 
 target = input(r"Enter file to be unzipped: ")
 handle = zipfile.ZipFile(target)
-handle.extractall("./Unzip file/Unzip file")
+handle.extractall("./Unzip file/Unzip files")
 handle.close()
